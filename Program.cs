31c3fabdb1bbedcba12256f095a473f400dd2c9b
Georgy Popov <95752544+georgypopov@users.noreply.github.com>
2023-06-25 21:42:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            double total = num * 7.61;
+            double discount = total * 0.18;
+            Console.WriteLine($"The final price is: {total - discount} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
